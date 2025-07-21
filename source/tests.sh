232c6a3a13cefd269d368529480963fe7db95e9f
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-07-21 15:19:04 +0300

    Update tests.sh

--- source/tests.sh
+++ source/tests.sh
@@ -13,14 +13,14 @@ fi
 passCounter=0
 failCounter=0
 
-strip_prompt() 
+stripPrompt() 
 { 
     
     sed '1{/^[^[:space:]]*\>\s*/d;}' 
 
 }
 
-run_test() 
+runTest() 
 {
 
     local name="$1"  
@@ -30,7 +30,7 @@ run_test()
     local out_file
     out_file=$(mktemp)
 
-    printf "%s\nexit\n" "$commands" | "$BIN" 2>/dev/null | strip_prompt | sed '/^$/d' >"$out_file"
+    printf "%s\nexit\n" "$commands" | "$BIN" 2>/dev/null | stripPrompt | sed '/^$/d' >"$out_file"
 
     if diff -u <(printf "%s\n" "$expected") "$out_file" >/dev/null; then
         printf '\e[32m[PASS]\e[0m %s\n' "$name"
@@ -46,13 +46,13 @@ run_test()
 
 }
 
-run_test "echo_simple"        "echo hello"                                       "hello"
-run_test "builtin_cd_pwd"     "mkdir -p tests_tmp; cd tests_tmp; pwd"            "$(pwd)/tests_tmp"
-run_test "variable_assign"    "FOO=bar; echo $FOO"                               "bar"
-run_test "pipeline"           "echo -e 'foo\nbar' | grep bar"                   "bar"
-run_test "redir_output"       "echo 42 > out.tmp; cat out.tmp"                  "42"
-run_test "redir_input"        "echo 'one' > in.tmp; wc -l < in.tmp"             "1"
-run_test "background_job"     "sleep 0.1 & echo done"                           "done"
+runTest "echo_simple"        "echo hello"                                       "hello"
+runTest "builtin_cd_pwd"     "mkdir -p tests_tmp; cd tests_tmp; pwd"            "$(pwd)/tests_tmp"
+runTest "variable_assign"    "FOO=bar; echo $FOO"                               "bar"
+runTest "pipeline"           "echo -e 'foo\nbar' | grep bar"                   "bar"
+runTest "redir_output"       "echo 42 > out.tmp; cat out.tmp"                  "42"
+runTest "redir_input"        "echo 'one' > in.tmp; wc -l < in.tmp"             "1"
+runTest "background_job"     "sleep 0.1 & echo done"                           "done"
 
 echo "--------------------------------------------------"
 echo "Total: $((passCounter+failCounter))  Pass: $passCounter  Fail: $failCounter"
