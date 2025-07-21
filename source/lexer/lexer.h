f399d6213ff8598509b4ac82e803ab2c9e032611
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-07-21 14:39:24 +0300

    Update lexer.h

--- source/lexer/lexer.h
+++ source/lexer/lexer.h
@@ -1,1 +1,29 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+typedef enum 
+{
+
+    TOK_WORD,           
+    TOK_PIPE,           
+    TOK_GT, 
+    TOK_LT,     
+    TOK_AND,            
+    TOK_OR,             
+    TOK_SEMI,           
+    TOK_LPAR, 
+    TOK_RPAR, 
+    TOK_AMP,  
+    TOK_HEREDOC,        
+    TOK_GTGT,  
+    TOK_EOF
+
+} token_t;
+
+void lexerInit(const char* line);
+token_t lexerPeek(void);            
+token_t lexerGet(void);             
+const char* lexerText(void);        
+
+#endif 
 
